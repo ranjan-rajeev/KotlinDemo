52c9232b55243fe2d472778e7be28257cdb2cf2b
Rajeev Ranjan <rajeev.ranjan@adityabirla.bank>
2019-08-30 17:22:19 +0530

    changes

--- app/src/main/java/com/horizonlabs/kotlindemo/utility/Util.kt
+++ app/src/main/java/com/horizonlabs/kotlindemo/utility/Util.kt
@@ -1,9 +1,15 @@
 package com.horizonlabs.kotlindemo.utility
 
 import android.content.Context
+import android.os.Build
 import android.provider.Settings
 import androidx.core.content.ContextCompat.getSystemService
 import android.telephony.TelephonyManager
+import android.util.Log
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class Util {
@@ -13,6 +19,22 @@ class Util {
                 context.getContentResolver(),
                 Settings.Secure.ANDROID_ID
             );
+        }
+
+        fun getCurrentTime(): String {
+            var answer: String = ""
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val current = LocalDateTime.now()
+                val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy. HH:mm:ss")
+                answer = current.format(formatter)
+            } else {
+                var date = Date();
+                date.time = Calendar.getInstance().timeInMillis
+                val formatter = SimpleDateFormat("dd MMM yyyy HH:mma")
+                answer = formatter.format(date)
+            }
+            return answer
         }
 
     }
